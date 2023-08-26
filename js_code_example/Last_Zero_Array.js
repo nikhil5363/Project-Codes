148b41b06bd9d918de4d143239f2bcd068e6fafb
Nikhil Kumar Mahto <nikhil.pyy@gmail.com>
2023-08-26 23:14:29 +0530

    JS Example Updated 26th Aug

--- js_code_example/Last_Zero_Array.js
+++ js_code_example/Last_Zero_Array.js
@@ -2,7 +2,7 @@ let l = [1, 0, 2345, 0, 13, 0, 8, 0, 445, 0, 23, 90, 0, 78, 0, 99, 0, 23];
 // console.log(l.length)
 
 
-// let arr = [...l.filter((i)=> i !==0),...l.filter((i)=> i ===0)]
+let arr = [...l.filter((i)=> i !==0),...l.filter((i)=> i ===0)]
 
 // console.log(arr)
 // console.log(arr.length)
@@ -10,12 +10,12 @@ let l = [1, 0, 2345, 0, 13, 0, 8, 0, 445, 0, 23, 90, 0, 78, 0, 99, 0, 23];
 
 // let arrr = []
 
-// for (let i of l) {
-//     let index = l.indexOf(i)
-//     if (i === 0 && l.includes(i)) {
-//         arrr.push(i)
-//         l.splice(index,i)
-//     }
-// }
-// console.log(arrr)
-// console.log(l)
+for (let i of l) {
+    let index = l.indexOf(i)
+    if (i === 0 && l.includes(i)) {
+        arrr.push(i)
+        l.splice(index,i)
+    }
+}
+console.log(arrr)
+console.log(l)
